b091cfa9abec7aca92d3a5432c1a919d08f90b60
Ramesh Chellaton <rameshchellaton@rc-macbook-old.lan>
2023-01-13 10:43:08 -0500

    count function works in Rmd but not in R

--- GRCh38bk.R
+++ GRCh38bk.R
@@ -4,5 +4,15 @@ library(seqinr)
 dna <- read.fasta(file="GRCh38.fasta", seqtype="DNA",
                   forceDNAtolower=TRUE)
 dna
-number_of_sequences = length(dna)
+number_of_sequences <- length(dna)
 print(paste("Number of sequences:", number_of_sequences))
+
+# getNames & getAnnotate (gets some description as well)
+
+names <- getName(dna)
+annotation <- getAnnot(dna)
+print(paste("Names:", names))
+print(paste("Annotations:", annotation))
+
+# Composition of the sequence(s)
+count(dna[[1]],1)
